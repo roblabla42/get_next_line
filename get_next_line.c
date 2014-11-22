d6d8e1fded450ce16ae1d2db905a4c1aa53a86c1
roblabla <robinlambertz.dev@gmail.com>
2014-11-22 17:38:06 +0100

    Fix segfaults.

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,15 @@
 /*   By: rlambert <rlambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/10 15:43:10 by rlambert          #+#    #+#             */
-/*   Updated: 2014/11/22 10:57:10 by roblabla         ###   ########.fr       */
+/*   Updated: 2014/11/22 17:36:53 by roblabla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
-#include "get_next_line.h"
-
+#include "get_next_line.h" 
 static ssize_t	ft_len_to_endline(int fd, t_buf *buf)
 {
 	if (buf->cursor >= buf->size)
@@ -22,11 +22,36 @@ static ssize_t	ft_len_to_endline(int fd, t_buf *buf)
 		buf->size = read(fd, buf->buf, BUFF_SIZE);
 		buf->cursor = 0;
 	}
+	if (buf->size == 0)
+		return (-1);
 	char *endline = ft_memchr(buf->buf + buf->cursor, '\n', buf->size - buf->cursor);
 	if (endline == NULL)
 		return (-1);
 	else
-		return (endline - buf->buf + buf->cursor);
+		return (endline - (buf->buf + buf->cursor));
+}
+
+static char	*ft_strnjoin(char *s1, const char *s2, size_t s2len)
+{
+	char	*new;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+	{
+		new = ft_strnew(s2len);
+		ft_memcpy(new, s2, s2len);
+	}
+	else if (s2 == NULL)
+		return (s1);
+	else
+	{
+		new = ft_strnew(ft_strlen(s1) + s2len);
+		ft_strcpy(new, s1);
+		ft_strncat(new, s2, s2len);
+		free(s1);
+	}
+	return (new);
 }
 
 static char	*read_til_next_line(int fd, t_buf *buf)
@@ -35,18 +60,17 @@ static char	*read_til_next_line(int fd, t_buf *buf)
 	ssize_t	len;
 
 	newstr = NULL;
-	while ((len = ft_len_to_endline(fd, buf)) == -1)
+	while ((len = ft_len_to_endline(fd, buf)) == -1 && buf->size > 0)
 	{
-		// TODO : Replace with own impl
-		newstr = realloc(newstr, ft_strlen(newstr) + buf->size - buf->cursor + 1);
-		ft_strncat(newstr, buf->buf + buf->cursor, buf->size - buf->cursor);
+		newstr = ft_strnjoin(newstr, buf->buf + buf->cursor, buf->size - buf->cursor);
 		buf->cursor = buf->size;
 	}
-	newstr = realloc(newstr, ft_strlen(newstr) + len + 1);
+	if (buf->size == 0)
+		return (newstr);
+	newstr = ft_strnjoin(newstr, buf->buf + buf->cursor, len);
 	if (newstr == NULL)
 		return (NULL);
-	ft_strncat(newstr, buf->buf + buf->cursor, len);
-	buf->cursor += len;
+	buf->cursor += len + 1;
 	return (newstr);
 }
 
@@ -57,32 +81,34 @@ int			get_next_line(int const fd, char **line)
 	t_avl			*cur;
 	char			*tmpline;
 
-	// DEBUG BLOCK
-	ft_putstr("Trying to read from ");
-	ft_putnbr(fd);
-	ft_putendl("");
-	// END DEBUG BLOCK
 	cur = ft_avlfind(tree, fd);
 	if (cur == NULL)
 	{
 		buf = (t_buf*)malloc(sizeof(t_buf));
+		if (buf == NULL)
+			return (-1);
 		buf->size = 0;
 		buf->cursor = 0;
-		cur = ft_avlnew(fd, (void*)buf, sizeof(t_buf));
+		cur = ft_avlnew(fd, NULL, 0);
 		if (cur == NULL)
+		{
+			free(buf);
 			return (-1);
+		}
+		cur->content = buf;
+		cur->content_size = sizeof(t_buf);
 		ft_avladd(&tree, cur);
 	}
-	// DEBUG BLOCK
-	ft_putstr("Got my cur : ");
-	ft_putnbr((size_t)cur);
-	ft_putendl("");
-	// END DEBUG BLOCK
+	else
+		buf = cur->content;
 	if ((tmpline = read_til_next_line(fd, buf)) == NULL)
-		return (-1);
+		if (buf->size == 0)
+			return (0);
+		else
+			return (-1);
 	else
 	{
 		*line = tmpline;
-		return (buf->size > 1 ? 1 : buf->size);
+		return (1);
 	}
 }
